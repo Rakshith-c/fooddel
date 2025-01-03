e682c1008d6e64b0c2e15daa46dc562c2ae6f2b7
Rakshith-c <rakshithranadheera27@gmail.com>
2025-01-03 21:56:53 +0530

    Update Navbar.jsx

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -9,20 +9,22 @@ import { StoreContext } from '../../Context/StoreContext';
 const NavBar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState('home');
   const navigate = useNavigate();
-  const { getTotalCartAmount, token,setToken } = useContext(StoreContext);
-const logout = () => {
-  localStorage.removeItem("token");
-  setToken(""); 
-  navigate("/")
-}
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(""); 
+    navigate("/");
+  }
+
   return (
     <div className='navbar'>
-      <Link to='/' ><img className="logo" src={assets.logo} alt="Logo" /></Link>
+      <Link to='/'><img className="logo" src={assets.logo} alt="Logo" /></Link>
       <ul className="navbar-menu">
-        <Link to='/' className={menu === 'home' ? 'active' : ''} onClick={() => setMenu('home')}>home</Link>
-        <a href='#explore-menu' className={menu === 'menu' ? 'active' : ''} onClick={() => setMenu('menu')}>menu</a>
-        <a href='#app-download' className={menu === 'mobile-app' ? 'active' : ''} onClick={() => setMenu('mobile-app')}>mobile-app</a>
-        <a href='#footer' className={menu === 'contact-us' ? 'active' : ''} onClick={() => setMenu('contact-us')}>contact-us</a>
+        <Link to='/' className={menu === 'home' ? 'active' : ''} onClick={() => setMenu('home')}>Home</Link>
+        <Link to='#explore-menu' className={menu === 'menu' ? 'active' : ''} onClick={() => setMenu('menu')}>Menu</Link>
+        <Link to='#app-download' className={menu === 'mobile-app' ? 'active' : ''} onClick={() => setMenu('mobile-app')}>Mobile App</Link>
+        <Link to='#footer' className={menu === 'contact-us' ? 'active' : ''} onClick={() => setMenu('contact-us')}>Contact Us</Link>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="Search Icon" />
@@ -36,9 +38,9 @@ const logout = () => {
           <div className='navbar-profile'>
             <img src={assets.profile_icon} alt="Profile Icon" />
             <ul className='navbar-profile-dropdown'>
-              <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="Bag Icon" />Orders</li>
-              <hr />
-              <li onClick={logout}><img src={assets.logout_icon} alt="Logout Icon" />Logout</li>
+              <li key="orders" onClick={() => navigate('/myorders')}><img src={assets.bag_icon} alt="Bag Icon" />Orders</li>
+              <hr key="divider" />
+              <li key="logout" onClick={logout}><img src={assets.logout_icon} alt="Logout Icon" />Logout</li>
             </ul>
           </div>
         )}
